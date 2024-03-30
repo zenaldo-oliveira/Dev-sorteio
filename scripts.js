16ee6564bc8d592d6cea5c9861a4884bca61ae47
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-03-30 09:04:52 -0400

    atualização trecho de codigo java operadores

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ function gererateNumber(){
     const min= Math.ceil(document.querySelector(".input-min").value)  //arredonda o numero para cima
     const max= Math.floor(document.querySelector(".input-max").value) //arredonda o numero para baixo
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;  //numeros aleatorios
 
-
-    alert(result)
+    if(max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min  //arredonda o numero para baixo
+    
+        alert(result)
+    }else {
+        alert("Valor maximo deve ser maior que o valor minimo")
+    }
 }
